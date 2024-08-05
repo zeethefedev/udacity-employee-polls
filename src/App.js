df95969bd86ced36a0830043b11684dd0dc43182
zeethefedev <zeethefrontenddev@gmail.com>
2024-08-05 15:35:31 +0700

    remove unused

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import LeaderBoard from "./pages/LeaderBoard";
